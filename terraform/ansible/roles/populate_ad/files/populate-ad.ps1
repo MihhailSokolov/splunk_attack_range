b08d030470cde9ceb20745a60d3d171c78e492d6
msokolov-roche <mihhail.sokolov@roche.com>
2024-09-23 15:49:22 +0200

    Populate AD with an ansible task

--- terraform/ansible/roles/populate_ad/files/populate-ad.ps1
+++ terraform/ansible/roles/populate_ad/files/populate-ad.ps1
@@ -1,27 +1,30 @@
-$domain = "attackrange.local"
-$numberOfUsers = 20
+$domain = $args[0]
+$numberOfUsers = $args[1]
+$username = $args[2]
+$password = $args[3] | ConvertTo-SecureString -asPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential($username,$password)
 
-$domainController = $(Get-ADDomainController -server $domain).HostName
+$domainController = $(Get-ADDomainController -server $domain -Credential $credential).HostName
 $rootPath = $(($domain.ToLower().Split(".") | % { "DC=" + $_}) -Join ',')
 
 Write-Host "Creating OUs..."
-New-ADOrganizationalUnit -Name "Groups" -Path "$rootPath"
-New-ADOrganizationalUnit -Name "Employees" -Path "$rootPath"
-New-ADOrganizationalUnit -Name "Finance" -Path "OU=Employees,$rootPath"
-New-ADOrganizationalUnit -Name "IT" -Path "OU=Employees,$rootPath"
-New-ADOrganizationalUnit -Name "Other" -Path "OU=Employees,$rootPath"
-New-ADOrganizationalUnit -Name "Systems" -Path "$rootPath"
-New-ADOrganizationalUnit -Name "Clients" -Path "OU=Systems,$rootPath"
-New-ADOrganizationalUnit -Name "Servers" -Path "OU=Systems,$rootPath"
+New-ADOrganizationalUnit -Name "Groups" -Path "$rootPath" -Credential $credential
+New-ADOrganizationalUnit -Name "Employees" -Path "$rootPath" -Credential $credential
+New-ADOrganizationalUnit -Name "Finance" -Path "OU=Employees,$rootPath" -Credential $credential
+New-ADOrganizationalUnit -Name "IT" -Path "OU=Employees,$rootPath" -Credential $credential
+New-ADOrganizationalUnit -Name "Other" -Path "OU=Employees,$rootPath" -Credential $credential
+New-ADOrganizationalUnit -Name "Systems" -Path "$rootPath" -Credential $credential
+New-ADOrganizationalUnit -Name "Clients" -Path "OU=Systems,$rootPath" -Credential $credential
+New-ADOrganizationalUnit -Name "Servers" -Path "OU=Systems,$rootPath" -Credential $credential
 Start-Sleep -Seconds 5
 
 Write-Host "Creating Users and Groups..."
-New-ADGroup -Name "ServerAdmins" -SamAccountName ServerAdmins -GroupCategory Security -GroupScope Global -DisplayName "ServerAdmins" -Path "OU=Groups,$rootPath" -ManagedBy (Get-ADUser Administrator)
-New-ADGroup -Name "ITSupport" -SamAccountName ITSupport -GroupCategory Security -GroupScope Global -DisplayName "ITSupport" -Path "OU=Groups,$rootPath" -ManagedBy (Get-ADUser Administrator)
+New-ADGroup -Name "ServerAdmins" -SamAccountName ServerAdmins -GroupCategory Security -GroupScope Global -DisplayName "ServerAdmins" -Path "OU=Groups,$rootPath" -ManagedBy (Get-ADUser Administrator) -Credential $credential
+New-ADGroup -Name "ITSupport" -SamAccountName ITSupport -GroupCategory Security -GroupScope Global -DisplayName "ITSupport" -Path "OU=Groups,$rootPath" -ManagedBy (Get-ADUser Administrator) -Credential $credential
 Start-Sleep -Seconds 5
 
 $searchBase = "OU=Employees,$rootPath"
-$OUs = Get-ADOrganizationalUnit -Server $domainController -SearchBase $searchBase -SearchScope OneLevel -Filter * | Select-Object -expand DistinguishedName
+$OUs = Get-ADOrganizationalUnit -Server $domainController -SearchBase $searchBase -SearchScope OneLevel -Filter * -Credential $credential | Select-Object -expand DistinguishedName
 
 1..$numberOfUsers | ForEach-Object {
     $response = Invoke-RestMethod -Uri "https://randomuser.me/api?inc=name,email"
@@ -32,7 +35,7 @@ $OUs = Get-ADOrganizationalUnit -Server $domainController -SearchBase $searchBas
     $userId = $parts[0] + $parts[1].Substring(0, 1)
     $userExists = $false
     try {
-        Get-ADUser -Server $domainController "$userId"
+        Get-ADUser -Server $domainController "$userId" -Credential $credential
         $userExists = $true
     }
     catch [Microsoft.ActiveDirectory.Management.ADIdentityResolutionException] {
@@ -42,7 +45,7 @@ $OUs = Get-ADOrganizationalUnit -Server $domainController -SearchBase $searchBas
         $randomNum = Get-Random -Minimum -1 -Maximum 10
         $userId += "$randomNum"
         try {
-            Get-ADUser -Server $domainController "$userId"
+            Get-ADUser -Server $domainController "$userId" -Credential $credential
             $userExists = $true
         }
         catch [Microsoft.ActiveDirectory.Management.ADIdentityResolutionException] {
@@ -60,12 +63,12 @@ $OUs = Get-ADOrganizationalUnit -Server $domainController -SearchBase $searchBas
 
     $OU = $OUs | Get-Random -Count 1
     try {
-        New-ADUser -Server $domainController -Name "$userId" -GivenName "$firstName" -Surname "$lastName" -SamAccountName "$userId" -DisplayName "$userId" -UserPrincipalName "$userId@$domain" -Path "$OU" -AccountPassword (ConvertTo-SecureString -AsPlainText "$password" -Force) -EmailAddress "$email" -Enabled $true
+        New-ADUser -Server $domainController -Name "$userId" -GivenName "$firstName" -Surname "$lastName" -SamAccountName "$userId" -DisplayName "$userId" -UserPrincipalName "$userId@$domain" -Path "$OU" -AccountPassword (ConvertTo-SecureString -AsPlainText "$password" -Force) -EmailAddress "$email" -Enabled $true -Credential $credential
     } catch [Microsoft.ActiveDirectory.Management.ADPasswordException] {
         $password = -join ((65..90) + (97..122) | Get-Random -Count 12 | % {[char]$_})
         $password += -join ((48..59) | Get-Random -Count 2 | % {[char]$_})
         $password += -join ($symbols | Get-Random -Count 2 | % {[char]$_})
-        New-ADUser -Server $domainController -Name "$userId" -GivenName "$firstName" -Surname "$lastName" -SamAccountName "$userId" -DisplayName "$userId" -UserPrincipalName "$userId@$domain" -Path "$OU" -AccountPassword (ConvertTo-SecureString -AsPlainText "$password" -Force) -EmailAddress "$email" -Enabled $true
+        New-ADUser -Server $domainController -Name "$userId" -GivenName "$firstName" -Surname "$lastName" -SamAccountName "$userId" -DisplayName "$userId" -UserPrincipalName "$userId@$domain" -Path "$OU" -AccountPassword (ConvertTo-SecureString -AsPlainText "$password" -Force) -EmailAddress "$email" -Enabled $true -Credential $credential
     }
     Write-Host "Created user $userId with password $password in $OU"
     Start-Sleep -Seconds 5
@@ -73,59 +76,65 @@ $OUs = Get-ADOrganizationalUnit -Server $domainController -SearchBase $searchBas
 
 
 Write-Host "Assigning Users to Groups"
-$serverAdminsGroup = Get-ADGroup ServerAdmins
-$itSupportGroup = Get-ADGroup ITSupport
+$serverAdminsGroup = Get-ADGroup ServerAdmins -Credential $credential
+$itSupportGroup = Get-ADGroup ITSupport -Credential $credential
 
-$ITUsers = Get-ADUser -Filter "*" -SearchBase "OU=IT,OU=Employees,$rootPath"
+$ITUsers = Get-ADUser -Filter "*" -SearchBase "OU=IT,OU=Employees,$rootPath" -Credential $credential
 For ($i=0; $i -lt ($ITUsers.Length * 2 / 3); $i++) {
     if ((Get-Random -Minimum 0 -Maximum 2) -gt 0) {
-        Add-ADGroupMember -Identity $itSupportGroup -Members $ITUsers[$i]
+        Add-ADGroupMember -Identity $itSupportGroup -Members $ITUsers[$i] -Credential $credential
     } else {
-        Add-ADGroupMember -Identity $serverAdminsGroup -Members $ITUsers[$i]
+        Add-ADGroupMember -Identity $serverAdminsGroup -Members $ITUsers[$i] -Credential $credential
     }
 }
 
-$users = Get-ADUser -Filter "*"
+$users = Get-ADUser -Filter "*" -Credential $credential
 
 Write-Host "Creating Computers..."
-$existingComputers = Get-ADComputer -Filter "*" -SearchBase "CN=Computers,$rootPath"
+$existingComputers = Get-ADComputer -Filter "*" -SearchBase "CN=Computers,$rootPath" -Credential $credential
 ForEach ($computer in $existingComputers) {
-    Move-ADObject -Identity $computer -TargetPath "OU=Servers,OU=Systems,$rootPath"
+    Move-ADObject -Identity $computer -TargetPath "OU=Servers,OU=Systems,$rootPath" -Credential $credential
 }
 
 For ($i=1; $i -le $users.Length; $i++) {
     $computerName = "CLIENT-WIN-$($i + 1)"
-    New-ADComputer -Name $computerName -SamAccountName $computerName -Path "OU=Clients,OU=Systems,$rootPath" -ManagedBy $itSupportGroup
+    New-ADComputer -Name $computerName -SamAccountName $computerName -Path "OU=Clients,OU=Systems,$rootPath" -ManagedBy $itSupportGroup -Credential $credential
 }
 
 For ($i=1; $i -le ($users.Length / 5); $i++) {
     $computerName = "SERVER-WIN-$i"
-    New-ADComputer -Name $computerName -SamAccountName $computerName -Path "OU=Servers,OU=Systems,$rootPath" -ManagedBy $serverAdminsGroup
+    New-ADComputer -Name $computerName -SamAccountName $computerName -Path "OU=Servers,OU=Systems,$rootPath" -ManagedBy $serverAdminsGroup -Credential $credential
 }
 
 Start-Sleep -Seconds 10
 
 Write-Host "Adding privilege escalation path..."
 
-$path = "AD:\$($itSupportGroup.DistinguishedName)"
-$acl = Get-Acl $path
+New-ADUser -Server $domainController -Name "billh" -GivenName "Bill" -Surname "Hardman" -SamAccountName "billh" -DisplayName "billh" -UserPrincipalName "$billh@$domain" -Path "OU=IT,OU=Employees,$rootPath" -AccountPassword (ConvertTo-SecureString -AsPlainText "PurpleSAR2024!" -Force) -EmailAddress "bill.hardman@$domain" -Enabled $true -Credential $credential
+Start-Sleep -Seconds 3
+Add-ADGroupMember -Identity $serverAdminsGroup -Members $(Get-ADUser "billh")
+
+$adDrive = New-PSDrive -Name DomainAD -PSProvider ActiveDirectory -Server $domainController -root "//RootDSE/" -Credential $credential
+
+$path = "$($adDrive.Name):\$($itSupportGroup.DistinguishedName)"
+$acl = Get-Acl $path -Credential $credential
 $accessrule = New-Object System.DirectoryServices.ActiceDirectoryAccessRule($serverAdminsGroup.sid, "GenericWrite", "Allow")
 $acl.AddAccessRule($accessrule)
-Set-Acl -AclObject $acl -Path $path
+Set-Acl -AclObject $acl -Path $path -Credential $credential
 
 ForEach ($user in $users) {
-    $path = "AD:\$($user.DistinguishedName)"
-    $acl = Get-Acl $path
+    $path = "$($adDrive.Name):\$($user.DistinguishedName)"
+    $acl = Get-Acl $path -Credential $credential
     $accessrule = New-Object System.DirectoryServices.ActiceDirectoryAccessRule($itSupportGroup, "ExtendedRight", "Allow")
     $acl.AddAccessRule($accessrule)
-    Set-Acl -AclObject $acl -Path $path
+    Set-Acl -AclObject $acl -Path $path -Credential $credential
 }
-ForEach ($user in $(Get-ADGroupMember "Domain Admins")) {
-    $path = "AD:\$($user.DistinguishedName)"
-    $acl = Get-Acl $path
+ForEach ($user in $(Get-ADGroupMember "Domain Admins" -Credential $credential)) {
+    $path = "$($adDrive.Name):\$($user.DistinguishedName)"
+    $acl = Get-Acl $path -Credential $credential
     $accessrule = New-Object System.DirectoryServices.ActiceDirectoryAccessRule($itSupportGroup, "ExtendedRight", "Allow")
     $acl.AddAccessRule($accessrule)
-    Set-Acl -AclObject $acl -Path $path
+    Set-Acl -AclObject $acl -Path $path -Credential $credential
 }
 
 Write-Host "Finished - login with one of the users from ServerAdmins group on the server to be compromised"
