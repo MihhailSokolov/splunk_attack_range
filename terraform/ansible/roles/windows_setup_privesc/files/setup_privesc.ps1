fe7feed4d49ddcc6f4503de73c888f8611b93a6b
msokolov-roche <mihhail.sokolov@roche.com>
2024-11-06 19:35:33 +0100

    Create a scheduled with admin instead of billh

--- terraform/ansible/roles/windows_setup_privesc/files/setup_privesc.ps1
+++ terraform/ansible/roles/windows_setup_privesc/files/setup_privesc.ps1
@@ -2,6 +2,9 @@ if (-not $env:computername.ToLower().Contains("win-2")) {
     exit
 }
 
+$user = $args[0]
+$password = $args[1]
+
 net user PurpleUser SecurePwd123 /add
 net localgroup "Remote Desktop Users" PurpleUser /add
 
@@ -13,7 +16,7 @@ net localgroup administrators ATTACKRANGE\ServerAdmins /add
 $action = New-ScheduledTaskAction -Execute "powershell" -Argument "-WindowStyle Hidden -File C:\Update.ps1"
 $trigger = New-ScheduledTaskTrigger -Once -At (Get-Date).Date -RepetitionInterval (New-TimeSpan -Minutes 1)
 $settings = New-ScheduledTaskSettingsSet
-Register-ScheduledTask -TaskName UpdateTask -TaskPath "\" -Action $action -Trigger $trigger -User "ATTACKRANGE\billh" -Password "PurpleSAR2024!" -Settings $settings -RunLevel Highest
+Register-ScheduledTask -TaskName UpdateTask -TaskPath "\" -Action $action -Trigger $trigger -User $user -Password $password -Settings $settings -RunLevel Highest
 
 $Scheduler = New-Object -ComObject "Schedule.Service"
 $Scheduler.Connect()
