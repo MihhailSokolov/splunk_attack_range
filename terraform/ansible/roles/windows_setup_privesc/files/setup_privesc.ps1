81960345c3a0d59d91764081a9445a6bbb665316
msokolov-roche <mihhail.sokolov@roche.com>
2024-11-27 20:00:03 +0100

    Make setup_privesc repeatable without errors

--- terraform/ansible/roles/windows_setup_privesc/files/setup_privesc.ps1
+++ terraform/ansible/roles/windows_setup_privesc/files/setup_privesc.ps1
@@ -5,22 +5,31 @@ if (-not $env:computername.ToLower().Contains("win-2")) {
 $user = $args[0]
 $password = $args[1]
 
-net user PurpleUser SecurePwd123 /add
-net localgroup "Remote Desktop Users" PurpleUser /add
+$UserID = Get-LocalUser -Name 'PurpleUser' -ErrorAction SilentlyContinue
+if (-not $UserID){
+    net user PurpleUser SecurePwd123 /add
+    net localgroup "Remote Desktop Users" PurpleUser /add
+}
+
 
 "Write-Host 'Updating...'" | Out-File C:\Update.ps1
 icacls C:\Update.ps1 /grant Everyone:F
 
-net localgroup administrators ATTACKRANGE\ServerAdmins /add
+if ((Get-LocalGroupMember Administrators).Name -notcontains 'ATTACKRANGE\ServerAdmins') {
+    net localgroup administrators ATTACKRANGE\ServerAdmins /add
+}
 
-$action = New-ScheduledTaskAction -Execute "powershell" -Argument "-WindowStyle Hidden -File C:\Update.ps1"
-$trigger = New-ScheduledTaskTrigger -Once -At (Get-Date).Date -RepetitionInterval (New-TimeSpan -Minutes 1)
-$settings = New-ScheduledTaskSettingsSet
-Register-ScheduledTask -TaskName UpdateTask -TaskPath "\" -Action $action -Trigger $trigger -User $user -Password $password -Settings $settings -RunLevel Highest
+$task = Get-ScheduledTask -TaskName "UpdateTask" -ErrorAction SilentlyContinue
+if (-not $task) {
+    $action = New-ScheduledTaskAction -Execute "powershell" -Argument "-WindowStyle Hidden -File C:\Update.ps1"
+    $trigger = New-ScheduledTaskTrigger -Once -At (Get-Date).Date -RepetitionInterval (New-TimeSpan -Minutes 1)
+    $settings = New-ScheduledTaskSettingsSet
+    Register-ScheduledTask -TaskName UpdateTask -TaskPath "\" -Action $action -Trigger $trigger -User $user -Password $password -Settings $settings -RunLevel Highest
 
-$Scheduler = New-Object -ComObject "Schedule.Service"
-$Scheduler.Connect()
-$task = $Scheduler.GetFolder("\").getTask("UpdateTask")
-$descriptor = $task.GetSecurityDescriptor(0xF)
-$descriptor = $descriptor + '(A;;GR;;;AU)'
-$task.SetSecurityDescriptor($descriptor, 0)
+    $Scheduler = New-Object -ComObject "Schedule.Service"
+    $Scheduler.Connect()
+    $task = $Scheduler.GetFolder("\").getTask("UpdateTask")
+    $descriptor = $task.GetSecurityDescriptor(0xF)
+    $descriptor = $descriptor + '(A;;GR;;;AU)'
+    $task.SetSecurityDescriptor($descriptor, 0)
+}
