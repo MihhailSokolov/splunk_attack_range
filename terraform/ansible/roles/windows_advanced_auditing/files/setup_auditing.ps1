ca4866871d3cac4d94c189f1ef8ed204867243d4
msokolov-roche <mihhail.sokolov@roche.com>
2024-12-05 21:42:26 +0100

    Add DPAPI Activity auditing

--- terraform/ansible/roles/windows_advanced_auditing/files/setup_auditing.ps1
+++ terraform/ansible/roles/windows_advanced_auditing/files/setup_auditing.ps1
@@ -1,4 +1,5 @@
 timeout 600
-auditpol /set /subcategory:"Handle Manipulation" /success:enable /failure:disable
-auditpol /set /subcategory:"Registry" /success:enable /failure:disable
-auditpol /set /subcategory:"SAM" /success:enable /failure:disable
+auditpol /set /subcategory:"Handle Manipulation" /success:enable /failure:enable
+auditpol /set /subcategory:"Registry" /success:enable /failure:enable
+auditpol /set /subcategory:"SAM" /success:enable /failure:enable
+auditpol /set /subcategory:"DPAPI Activity" /success:enable /failure:enable
